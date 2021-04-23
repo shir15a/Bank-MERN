bfd4d41ec72fa27ec7e4efc025fe6762a8f27d67
shir15a <shir95admon@gmail.com>
2021-04-23 18:52:03 +0300

    .

--- src/server/controllers/transactions.controller.js
+++ src/server/controllers/transactions.controller.js
@@ -1,4 +1,4 @@
-const accountModel = require("../models/account.model");
+const accountModel = require("../models/transactions.model");
 
 // deposit
 const updateCash = async (req, res) => {
@@ -22,32 +22,7 @@ const updateCash = async (req, res) => {
     }
 };
 
-const transferMoney = async (req, res) => {
-    const updates = Object.keys(req.body);
-    console.log(updates, 'updates');
-    const allowedUpdates = ['from', 'to', 'account.cash'];
-    const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
-    console.log(isValidOperation,'isValidOperation');
-    if (!isValidOperation) {
-        return res.status(400).send({ error: 'Invalid updates !' })
-    }
-    console.log(req.body.from);
-    try {
-        const fromUser = await accountModel.findByIdAndUpdate(req.body.from, {"account.cash": cash - req.body.amount}, { new: true, runValidators: true });
-        const toUser = await accountModel.findByIdAndUpdate(req.body.to, {"account.cash": cash + req.body.amount}, { new: true, runValidators: true });
-
-        if (!fromUser || !toUser) {
-            return res.status(404).send()
-        }
-        // res.send(product)
-    }
-    catch (error) {
-        res.status(400).json(error)
-    }
-};
-
 module.exports = {
-    transferMoney : transferMoney,
     deposit : updateCash
 };
 
